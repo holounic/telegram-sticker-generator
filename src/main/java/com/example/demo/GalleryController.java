686275f2c22ee9bc1676839b3daae82a70666782
holounic <mia.ilminsckaya@yandex.ru>
2020-12-13 23:07:04 +0300

    add download button

--- src/main/java/com/example/demo/GalleryController.java
+++ src/main/java/com/example/demo/GalleryController.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GalleryController {
-
+    @RequestMapping
+    
 }
--- src/main/java/com/example/demo/ImageController.java
+++ src/main/java/com/example/demo/ImageController.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import com.example.demo.tools.ImageProcessor;
-import com.example.demo.tools.StorageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +8,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ImageController {
-
-    @Autowired
-    StorageService service;
 
     @RequestMapping(value="/process", method = RequestMethod.POST, produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<byte[]> postImageToProcess(@RequestParam("file") MultipartFile image) {
